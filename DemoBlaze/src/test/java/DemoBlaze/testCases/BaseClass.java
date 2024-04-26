31d2521d787ef85155555f4e6f2f8c75f42e4c5b
Mansi <Mansi@DESKTOP-Q6H280O.home>
2024-04-26 13:01:36 -0400

    fix:added DeleteCookie method

--- DemoBlaze/src/test/java/DemoBlaze/testCases/BaseClass.java
+++ DemoBlaze/src/test/java/DemoBlaze/testCases/BaseClass.java
@@ -66,6 +66,7 @@ public class BaseClass {
 		}
 
 		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.get(baseURL);
 
